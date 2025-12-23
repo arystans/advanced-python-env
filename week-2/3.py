80464d3abefe647fb7f65eb308fd9b222b392f11
Momongo <119149437+arystans@users.noreply.github.com>
2025-12-23 15:46:29 +0100

    Implement basic arithmetic solver for x

---
+++ week-2/3.py
@@ -0,0 +1,14 @@
+s = input().strip()
+a, op, b, _, c = s
+def val(x):
+    return int(x) if x != 'x' else None
+if a == 'x':
+    b, c = int(b), int(c)
+    x = c - b if op == '+' else c + b
+elif b == 'x':
+    a, c = int(a), int(c)
+    x = c - a if op == '+' else a - c
+else:  # c == 'x'
+    a, b = int(a), int(b)
+    x = a + b if op == '+' else a - b
+print(x)
