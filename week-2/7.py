acb296f7c67c299f9a48192607e49afb1d331829
Momongo <119149437+arystans@users.noreply.github.com>
2025-12-23 16:55:58 +0100

    Update 7.py

--- week-2/7.py
+++ week-2/7.py
@@ -10,17 +10,18 @@ for k in freq:
     print(k, freq[k])
 max_item = None
 max_count = 0
+t = ""
 for k in freq:
     if freq[k] > max_count:
         max_count = freq[k]
         max_item = k
 print("\nMost popular item:", max_item)
-print("\nPurchased once:")
 for k in freq:
     if freq[k] == 1:
-        print(k, end=" ")
+        t += k + " "
+print("\nPurchased once:",t)
+
 sorted_items = sorted(freq.items(), key=lambda x: x[1], reverse=True)
 print("\nSorted by frequency :")
 for item, count in sorted_items:
     print(item, count)
-
